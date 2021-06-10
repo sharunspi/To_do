22b4a102411d64e94a80bb649cc6f2f50c5f60a9
sharunspi <sharunkk1997@gmail.com>
2021-06-10 09:28:35 +0530

    changed fonr

--- to-do-front/src/Components/InputField.js
+++ to-do-front/src/Components/InputField.js
@@ -9,7 +9,7 @@ export default function InputField(props) {
     return (
         <div className='input-container'>
            <form onSubmit={e=>{newEventAdded(e)}}>
-            <input type="text" placeholder='type new task'/>
+            <input type="text" placeholder='Type new task'/>
            </form>
         </div>
     )
