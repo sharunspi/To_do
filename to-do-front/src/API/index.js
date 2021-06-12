f62796512a7a7d99557b89bdd5915900ca230dc0
sharunspi <sharunkk1997@gmail.com>
2021-06-12 11:02:14 +0530

    added drag delete option

--- to-do-front/src/API/index.js
+++ to-do-front/src/API/index.js
@@ -1,6 +1,28 @@
 import axios from 'axios'
+import {backendUrl} from '../config'
 export const getAllTask = async ()=>{
-    let data = await axios.get('http://localhost:3001/get_list',{headers:{
+    let data = await axios.get(`${backendUrl}/get_list`,{headers:{
+        'Content-Type':'application/json'
+    }})
+    return data
+}
+
+export const deteleATask = async (id)=>{
+    let data = await axios.delete(`${backendUrl}/task/${id}`,{headers:{
+        'Content-Type':'application/json'
+    }})
+    return data
+}
+
+export const deteleAllTask = async ()=>{
+    let data = await axios.delete(`${backendUrl}/all`,{headers:{
+        'Content-Type':'application/json'
+    }})
+    return data
+}
+
+export const addNewTask = async (task)=>{
+    let data = await axios.post(`${backendUrl}/add_new_task`,task,{headers:{
         'Content-Type':'application/json'
     }})
     return data
--- to-do-front/src/App.js
+++ to-do-front/src/App.js
@@ -2,19 +2,35 @@ import React,{useState,useEffect} from 'react'
 import Item from './Components/Item'
 import InputField from './Components/InputField'
 import Header from './Components/Header'
-import {getAllTask } from './API'
+import {getAllTask,addNewTask } from './API'
+function createHash(ta){
+    return ta+Math.random()
+}
 export default function App() {
     const [newTask,setNewTask] = useState('')
     const [taskList,setTaskList] = useState([])
     const newTaskAddedFunc = task =>{
+        let taskId = createHash(task)
         setNewTask(task)
-        setTaskList([...taskList,{
-            text:task,
-            complete:false
-        }])
+        let taskDetails = {
+            taskId:taskId,
+            task:task,
+            status:false
+        }
+     
+        addNewTask(taskDetails).then(res=>{
+            console.log(res)
+            setTaskList([...taskList,taskDetails])
+        }).catch(err=>{
+            console.log(err)
+        })
     }
     const setStatus = val =>{
         console.log(val)
+    }
+    const deleteElementFun = eleId =>{
+        let newList = taskList.filter(task => task.taskId != eleId)
+        setTaskList(newList)
     }
     useEffect(()=>{
     getAllTask().then(res=>{
@@ -33,7 +49,7 @@ export default function App() {
              {
                      taskList.length >0 && 
                      taskList.map((taskOb,index)=>{
-                         return <Item status={setStatus} key={taskOb.taskId} id={taskOb.taskId} complete={taskOb.status} text={taskOb.task}/>
+                         return <Item deleteElement={deleteElementFun} status={setStatus} key={taskOb.taskId} id={taskOb.taskId} complete={taskOb.status} text={taskOb.task}/>
                      })
                  }
              </ul>
